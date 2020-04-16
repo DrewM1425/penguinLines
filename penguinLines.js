e94b1fce58a77f4d95cd6c93f272c855707bcc50
DrewM1425 <dmeaux04@gmail.com>
2020-04-16 12:44:35 -0400

    Starting up

---
+++ penguinLines.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+var penguinPromise = d3.json("classData.json"); //Promise to get the data
+
+var successFcn = function(penguins) //If the data is successfully collected
+{
+    console.log("Data Collected:",penguins);
+}
+
+var failureFcn = function(errorMsg) //If there was an error
+{
+    console.log("Something went wrong",errorMsg);
+}
+
+penguinPromise.then(successFcn,failureFcn);
